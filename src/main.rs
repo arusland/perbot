81223574fbf6021d9ecb40ecd19059d99a08e3ff
Ruslan A. <ruslan.absalyamov@gmail.com>
2026-03-06 22:33:41 +0100

    More deatailed log format

--- src/main.rs
+++ src/main.rs
@@ -263,7 +263,25 @@ fn init_logger() {
             flexi_logger::Naming::Timestamps,
             flexi_logger::Cleanup::KeepLogFiles(365),
         )
+        .format_for_files(log_format)
+        .format_for_stdout(log_format)
         .duplicate_to_stdout(flexi_logger::Duplicate::All)
         .start()
         .expect("Failed to start logger");
+}
+
+fn log_format(
+    w: &mut dyn std::io::Write,
+    now: &mut flexi_logger::DeferredNow,
+    record: &log::Record,
+) -> std::io::Result<()> {
+    write!(
+        w,
+        "[{}] {:5} [{}:{}] {}",
+        now.format("%Y-%m-%d %H:%M:%S"),
+        record.level(),
+        record.module_path().unwrap_or("<unknown>"),
+        record.line().unwrap_or(0),
+        record.args()
+    )
 }
